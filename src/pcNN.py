10176323a52ab0b9b79aca8089d3595913a91087
Morten Vester Pedersen <mortenvester1@gmail.com>
2017-08-31 18:56:34 -0400

    initial commit

---
+++ src/pcNN.py
@@ -0,0 +1,26 @@
+import os
+import pdb
+import sys
+
+import pylab
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+class pcNN:
+    def __init__(self):
+        self.train = None
+        self.test = None
+
+   def forward(x):
+	return x
+
+   def
