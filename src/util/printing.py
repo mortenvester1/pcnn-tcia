a4f33de83487643d8bdd4dd6c5564e65cf91bffe
Morten Vester Pedersen <mortenvester1@gmail.com>
2017-11-01 21:09:53 -0400

    udpate

--- src/util/printing.py
+++ src/util/printing.py
@@ -1,12 +1,10 @@
-OUT_WIDTH = 80
-
-def print_header(header):
+def print_header(header, OUT_WIDTH = 80):
     prl = (OUT_WIDTH//2-len(header)//2) - 1
     prr = OUT_WIDTH - prl - len(header) - 2
     print("#" + " "*prl + header + " "*prr + "#")
     return
 
 
-def print_border():
+def print_border(OUT_WIDTH = 80):
     print("-"*OUT_WIDTH)
     return
